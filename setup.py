729ada00e2b15f0f0907b0a0f3e5e344b5236c8c
nolash <dev@holbrook.no>
2021-04-14 09:28:50 +0200

    Remove dependency breaker requirements

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ f.close()
 
 setup(
         name="crypto-dev-signer",
-        version="0.4.14b1",
+        version="0.4.14b2",
         description="A signer and keystore daemon and library for cryptocurrency software development",
         author="Louis Holbrook",
         author_email="dev@holbrook.no",
